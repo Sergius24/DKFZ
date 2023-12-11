f91030572383269efb6623a14891496c4386b388
Sergius24 <153516077+Sergius24@users.noreply.github.com>
2023-12-11 15:58:43 +0100

    Add files via upload

---
+++ Colorbalance1.java
@@ -0,0 +1,47 @@
+package com.mycompany.colorbalance1;
+
+import java.io.*;
+import java.util.*;
+import java.nio.file.*;
+
+/**
+ *
+ * @author sergey
+ */
+public class Colorbalance1 {
+    
+    public static void main(String[] args) throws IOException {
+        System.out.println(" red / green");
+        List <String> allLines = Files.readAllLines(Paths.get("Sequences"));
+ 
+        for (int i=0; i<maxString(allLines); i++){
+            int red = 0;
+            int green = 0;
+            for (int j=0; j<allLines.size(); j++){
+                if (i<allLines.get(j).replaceAll(" ", "").length()){
+                switch (Character.toUpperCase(allLines.get(j).replaceAll(" ", "").charAt(i))) {
+                    case 'T':
+                        green++;
+                        break;
+                    case 'C':
+                        red++;
+                        break;
+                    case 'A':
+                        green++;
+                        red++;
+                        break;                
+                    } 
+                }
+            }
+            System.out.println(String.format("%s. %s / %s", i+1, red, green));           
+        }
+    }
+    static private int maxString(List<String> strings){
+        int maxLength = strings.get(0).replaceAll(" ", "").length();
+        for (String str: strings)
+            if (str.replaceAll(" ", "").length()>maxLength)
+                maxLength=str.replaceAll(" ", "").length();
+        return maxLength;
+    }
+}
+
